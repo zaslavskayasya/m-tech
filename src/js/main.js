27c76596daa304002661a9bbacdb711e5e280410
Asya <web.zaslavskaya@gmail.com>
2023-08-14 17:25:11 +0300

    menu pluses

--- src/js/main.js
+++ src/js/main.js
@@ -138,20 +138,21 @@ closeMenuBtn.addEventListener('click', ()=>{
 
 let dropsBtns = document.querySelectorAll('.dropup');
 
+dropsBtns.forEach((item) => {
+  item.addEventListener('click', () => {
+      // Перебираємо всі пункти меню
+      dropsBtns.forEach((otherItem) => {
+          if (otherItem !== item && otherItem.classList.contains("open")) {
+              otherItem.classList.remove("open");
+          }
+      });
 
-dropsBtns.forEach((item, e)=>{
-    item.addEventListener('click', (e)=>{
-        console.log(item);
-        
-        if(item.classList.contains("open")){
-            item.classList.remove("open");
-        } else {
-            item.classList.add("open");
-
-        }
-
-    })
+      // Тогглімо "open" клас для поточного пункту меню
+      if (item.classList.contains("open")) {
+          item.classList.remove("open");
+      } else {
+          item.classList.add("open");
+      }
+  });
 });
-
-
 
