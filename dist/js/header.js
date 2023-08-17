d8430b9391e49fbaa8d9adc49f898447da1d17b3
Asya <web.zaslavskaya@gmail.com>
2023-08-17 15:42:15 +0300

    page catalog

--- dist/js/header.js
+++ dist/js/header.js
@@ -2,21 +2,17 @@ console.log('header loaded')
 
 let openMenuBtn = document.querySelector('.menu-button');
 let closeMenuBtn = document.querySelector('.close-button');
-
 let headerIs = document.querySelector('.header');
 
 
-console.log(openMenuBtn);
-
-
-
 openMenuBtn.addEventListener('click', ()=>{
-    headerIs.classList.add('open-header')
+    headerIs.classList.add('open-header');    
 });
 
 closeMenuBtn.addEventListener('click', ()=>{
     headerIs.classList.remove('open-header')
 });
+
 
 let dropsBtns = document.querySelectorAll('.dropup');
 
@@ -26,14 +22,17 @@ dropsBtns.forEach((item) => {
       dropsBtns.forEach((otherItem) => {
           if (otherItem !== item && otherItem.classList.contains("open")) {
               otherItem.classList.remove("open");
+              headerIs.classList.remove('open'); 
           }
       });
 
       // Тогглімо "open" клас для поточного пункту меню
       if (item.classList.contains("open")) {
           item.classList.remove("open");
+          headerIs.classList.remove('open'); 
       } else {
           item.classList.add("open");
+          headerIs.classList.add('open'); 
       }
   });
 });
