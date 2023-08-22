e736801767119c3b67e31f194f8a187c11970a5b
Asya <web.zaslavskaya@gmail.com>
2023-08-22 12:10:05 +0300

    one page, desctop

--- src/js/header.js
+++ src/js/header.js
@@ -37,34 +37,3 @@ dropsBtns.forEach((item) => {
   });
 });
 
-
-
-class Employee {
-    salary = 1200;
-    static bonus = 300;
-    constructor(position){
-        this.position = position;
-    }
-    getSallary(){
-        return this.salary;
-    }
-    static getBonus(){
-        return this.bonus;
-    }
-}
-
-const employee = new Employee("developer");
-
-
-employee.bonus;
-
-
-function Example({someProp}) {
-    function doSomething(){
-        console.log(someProp);
-    }
-
-    useEffect(()=>{
-        doSomething();
-    }, [])
-}
--- src/js/one.js
+++ src/js/one.js
@@ -1,1 +1,130 @@
-console.log('one page ')
+console.log('one page ');
+
+let catalogSlider = $('.main-slider')
+
+catalogSlider.slick({
+  dots: false,
+  arrows: false,
+  // slidesToScroll: 1,
+  slidesToShow: 1,
+  asNavFor: '.preview-slider',
+  focusOnSelect: true,
+  infinite: false,
+});
+
+
+let sliderNavCatalog = $('.preview-slider');
+
+sliderNavCatalog.slick({
+  asNavFor: '.main-slider',
+  dots: false,
+  arrows: false,
+  centerMode: false,
+  focusOnSelect: true,
+  variableWidth: false,
+  slidesToShow: 3,
+  infinite: false,
+});
+
+
+let successSlider = $('.sccess-slider');
+
+successSlider.slick({
+  dots: false,
+  arrows: false,
+  centerMode: false,
+  focusOnSelect: true,
+  variableWidth: false,
+  slidesToShow: 3,
+  infinite: false,
+});
+
+$(".prev-btn").click(function () {
+    successSlider.slick("slickPrev");
+});
+
+$(".next-btn").click(function () {
+    successSlider.slick("slickNext");
+});
+
+
+$(".zoom-images img").each((idx, el) => {
+    $(el).ezPlus({
+
+    });
+});
+
+// $('#zoom_01').ezPlus({
+//     lenszoom: true,
+//     scrollZoom: true,
+//     zoomType: 'lens',
+//     // lensShape: 'round',
+//     lensSize:200
+//     // zoomType: 'inner',
+//     // cursor: 'crosshair'
+// });
+
+let tabButtons = document.querySelectorAll('.tab-btn');
+let contentList = document.querySelectorAll('.content-item');
+
+
+console.log(tabButtons);
+
+
+// tabButtons.addEventListener('click', (el)=>{
+//         console.log('click');
+// })
+
+tabButtons.forEach((el)=>{
+
+    el.addEventListener('click', ()=>{
+
+        tabButtons.forEach((e)=>{
+            e.classList.remove('active');
+        })
+
+        el.classList.add('active');
+
+        let activateTab = el.dataset.tab;
+        let activateEl = document.getElementById(activateTab);
+
+        contentList.forEach((ar)=>{
+            ar.classList.remove('active');
+        })
+
+        activateEl.classList.add('active');
+
+        console.log(activateEl);        
+
+    });
+
+})
+
+
+$('.slider-left').slick({
+    slidesToShow: 1,
+    // centerPadding: '70px',
+    // variableWidth: true,
+    dots: false,
+    arrows: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          centerMode: false,
+          dots: false
+
+        }
+      },
+    ]
+});
+$('.left-gallery > .custom-prev-button').click(function(){
+    $(' .slider-left').slick('slickPrev');
+  });
+
+  $('.custom-next-button').click(function(){
+    $('.left-gallery >  .slider-left').slick('slickNext');
+  });
