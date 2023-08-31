b961d79df0647b87ff8c51e9afde89679af2ad9c
Asya <web.zaslavskaya@gmail.com>
2023-08-31 23:00:33 +0300

    percision

--- src/js/main.js
+++ src/js/main.js
@@ -83,8 +83,8 @@ $('#scene').mousemove(function(e) {
     currentMousePos.x = e.pageX;
     currentMousePos.y = e.pageY;
   
-  $('#circle').css('-webkit-mask-position-x', currentMousePos.x - 100 );
-  $('#circle').css('-webkit-mask-position-y', currentMousePos.y - 100 )
+  $('#circle').css('-webkit-mask-position-x', currentMousePos.x - 85 );
+  $('#circle').css('-webkit-mask-position-y', currentMousePos.y - 85 )
 });
 
 
